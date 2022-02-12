6371dfd28d7b5fbc7e05909a3cc49f6e3622c3c9
Nishit Kumar <nishitkumar@Nishits-MacBook-Air.local>
2022-02-12 14:39:10 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,69 @@ import React, { Component} from "react";
 import {hot} from "react-hot-loader";
 import "./App.css";
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
-
-const user = {
-  firstName: 'Nishit',
-  lastName: 'Kumar'
-};
-
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, {formatName(user)}! </h1>
+const List = props => {
+  return props.list.map( function( item ) {
+    return (
+      <div key={ item.objectID }>
+        <span>
+          <a href={ item.url }>{ item.title }</a>
+        </span>
+        <span>{ item.author }</span>
+        <span>{ item.num_comments }</span>
+        <span>{ item.points }</span>
       </div>
     );
-  }
+  } );
 }
+
+  
+const App = () => {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+    return(
+      <div className="App">
+        <h1> My Hacker Stories</h1>
+        <Search />
+        <hr />
+        <List list = { stories } />
+      </div>
+    );
+}
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+    </div>
+  );
+};
 
 export default hot(module)(App);
